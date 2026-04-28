6a56caec7794f1a3110422f803d303aad9a60a3e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-28 06:51:53 -0400

    fix typo

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://asphyxia-core.github.io/img/core-logo.png
-export DESKTOP=DUMMYY
+export DESKTOP=DUMMY
 export MAIN_BIN=asphyxia-core
 
 # Deploy dependencies
