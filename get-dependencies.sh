dbde2eaa589f02d99fa029bfddf88ceb57747122
Samueru-sama <xdglawyer@outlook.com>
2026-04-28 16:04:13 -0400

    add plugins

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -38,3 +38,9 @@ awk -F'/' '/Location:/{print $(NF-1); exit}' /tmp/download.log > ~/version
 # asphyxia-core gets broken by strip
 # save it to /tmp to restore it later
 cp -v ./asphyxia-core /tmp
+
+# add plugins
+git clone --depth 1 https://github.com/asphyxia-core/plugins ./_plugins
+cp -rv ./_plugins/* ./plugins
+rm -rf ./_plugins
+
